ba1e093edb01e05261a67834804a040f18e67846
BHASHITHA <75405197+ImBhashitha@users.noreply.github.com>
2021-04-03 20:33:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
  SOURCE="https://github.com/ImBhashitha/ForwardTagRemoverBot"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©NET.HACKER BOTs 🇱🇰"
- HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
+ START_TEXT="Hi [{}](tg://user?id={})\n*I am A Forward Tag remover Bot.*\n*Send */help *To Know What I Can Do \n ©NET.HACKER BOTs 🇱🇰*"
+ HELP_TEXT="*Forward Me A File,Video,Audio,Photo or Anything And *\n*I will Send You the File Back*\n\n`*How to Set Caption?*`\n*Reply Caption to a File,Photo,Audio,Media*"
 	
